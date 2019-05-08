8cd8aa2cfad21f29862d361a212fcf7287876ed5
Tristan WAGNER <t.wagner@adveez.com>
2019-05-08 02:31:17 +0200

    feat(quests): added quest with multiple parts

--- index.js
+++ index.js
@@ -37,6 +37,11 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('error', error => {
+  console.log('Discord api client error', error)
+})
+
+//discord users messages entry point
 client.on('message', async msg => {
   let query = msg.content.split(' ').slice(1).join(' ')
   switch (true) {
@@ -55,8 +60,8 @@ client.on('message', async msg => {
 });
 
 const handleQuestSearch = (query, msg) => {
-  let match = findMatch(query, quest.index)
-  msg.reply(`Found quest ${quest.index[match]}\n ${quest.db.concat(match)}`)
+  let matches = findMatches(query, quest.index)
+  msg.reply(`Found ${matches.length} quests :\n`.concat(...matches.map((match, index) => `\t- ${matches.length > 1 ? quest.index[match[2]].concat(' Part ', index + 1) : quest.index[match[2]]}\n\t  ${quest.db.concat(match[2], '\n')}`)))
 }
 
 const handleNpcSearch = (query, msg) => {
@@ -70,8 +75,9 @@ const handleItemSearch = async (query, msg) => {
   console.log(`Found name : ${item.index[match]}`)
   msg.reply(`Found item ${item.index[match]}\n ${item.db.concat(match)}`)
   let filepath = cachePath.concat(match, '.png')
+  //if snapshot is not in cache
   if (!fs.existsSync(filepath)) {
-    await takeImage(match)
+    await takeSnapshot(match)
   }
   msg.reply(`Tooltip of ${item.index[match]}`, new Attachment(filepath))
 }
@@ -84,6 +90,8 @@ const initData = () => {
     })
   )
 }
+
+//fetch and store csv data for fuzzy search
 const fetchEntityData = ({ headers, fp, index }) => {
   let stream = fs.createReadStream(fp)
   return new Promise((resolve, reject) => {
@@ -93,6 +101,7 @@ const fetchEntityData = ({ headers, fp, index }) => {
       .fromStream(stream, {})
       .on('data', data => {
         if (i === 0) {
+          //keys are [id, name]
           keys = headers.map(header => {
             return data.indexOf(header)
           })
@@ -106,20 +115,35 @@ const fetchEntityData = ({ headers, fp, index }) => {
       .on('end', () => {
         resolve()
       })
+      .on('error', (err) => {
+        reject(err)
+      })
   })
 }
 
+//return only the matched id
 const findMatch = (str, data) => {
   let matches = fuzz.extract(str, data, { scorer: fuzz.WRatio })
+  console.log('matches', str, matches)
   return matches[0][2]
 }
 
-const takeImage = (id) => {
-  console.log('Taking image for item id:', id, 'in url', item.db.concat(id))
+//return a batch of matches with the same names, for quests with multiple parts
+//matches is a list of [choice, score, id]
+const findMatches = (str, data) => {
+  let matches = fuzz.extract(str, data, { scorer: fuzz.WRatio })
+  console.log('matches', str, matches)
+  //take all matches with same name as first match and also sort it by id so hoppefully we'll get the parts right
+  return matches.filter(match => match[0] === matches[0][0]).sort((a, b) => a[2] - b[2])
+}
+
+//take item's tooltip snapshot and save it to cache folder
+const takeSnapshot = (id) => {
+  console.log('Taking snapshot for item id:', id, 'in url', item.db.concat(id))
   return new Promise((resolve, reject) => {
     webshot(item.db.concat(id), cachePath.concat(id, '.png'), { captureSelector: '.tooltip' }, (err) => {
       if (err) {
-        console.log('Error while taking image for item id', id, err)
+        console.log('Error while taking snapshot for item id', id, err)
         reject(err)
       }
       resolve()
@@ -127,7 +151,8 @@ const takeImage = (id) => {
   })
 }
 
+//pull the trigger
 initData()
   .then(() => {
-    client.login('NTc1MTAwMzExOTkyMTM5ODA2.XNDD5w.QNx4SNd3aLfusx4AWCUrNrWCHWQ');
+    return client.login('NTc1MTAwMzExOTkyMTM5ODA2.XNDD5w.QNx4SNd3aLfusx4AWCUrNrWCHWQ')
   })
